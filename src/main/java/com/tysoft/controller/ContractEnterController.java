3c39e5617e712ec0be56a1d309d1acb768114030
BearBear <154929592@qq.com>
2018-12-28 17:26:47 +0800

    12-28第三次上传

--- src/main/java/com/tysoft/controller/ContractEnterController.java
+++ src/main/java/com/tysoft/controller/ContractEnterController.java
@@ -17,15 +17,9 @@ public class ContractEnterController extends BaseController{
 		
 		//合同签订界面
 		@RequestMapping("test")
-		public void signContractView(HttpServletRequest request,Map<String,Object> map) throws Exception {
-			System.out.println("------------数据库连接测试------------");
-			Annex annex=annexService.findAnnexById("402881e666be97330166be9a85310000");
-			if(annex!=null) {
-				String annexName=annex.getExtendName();
-				System.out.println("数据库连接成功 查到的数据:"+annexName);
-			}
-		
-		
+		public String signContractView(HttpServletRequest request,Map<String,Object> map) throws Exception {
+	
+			return "/login";
 		}
 		
 		
